47e89f65c68485f1160f7b9cde9a9f9a31d95b9f
Mousashi7 <99486825+Mousashi7@users.noreply.github.com>
2022-05-31 20:13:40 +0300

    modefication added

--- routes/apiRoutesV1.js
+++ routes/apiRoutesV1.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-// var usersController = require("../controllers/usersController");
 
 router.use("/users", require("../lib/admins/routes"));
 
@@ -9,23 +8,5 @@ router.use("/instructors", require("../lib/instructors/routes"));
 router.use("/students", require("../lib/students/routes"));
 
 router.use("/tests", require("../lib/tests/routes"));
-
-router.use("/questions", require("../lib/questions/routes"));
-
-router.use("/options", require("../lib/answers_options/routes"));
-
-router.use("/session", require("../lib/session-tests/routes"));
-
-// router.use("/students", require("../lib/students/routes"));
-
-// router.use("/tests", require("../lib/tests/routes"));
-
-// router.use("/questions", require("../lib/questions/routes"));
-
-// router.use("/classrooms", require("../lib/classrooms/routes"));
-
-// router.use("/answers_options", require("../lib/answers_options/routes"));
-
-// router.use("/session_tests", require("../lib/session_tests/routes"));
 
 module.exports = router;
