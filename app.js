ad0549aa3c50300b151fb4ce8edc7cbc19164bc8
Mousashi7 <99486825+Mousashi7@users.noreply.github.com>
2022-06-13 20:07:58 +0300

    one test for every student

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
